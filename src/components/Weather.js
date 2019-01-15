5c2fee7e80c59740c571dd39289678d8cfe9791d
github username <github email address>
2019-01-15 16:22:20 -0500

    successfully displaying weather and icon

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import {
+  WiCloudUp,
+  WiCloudy,
+  WiSnowflakeCold,
+  WiDaySunnyOvercast,
+  WiDaySunny,
+  WiDayShowers
+} from "weather-icons-react";
 
 class Weather extends Component {
+  chooseIcon = () => {
+    let type = this.props.weather.currently.icon;
+    if (type.includes("partly-cloudy")) {
+      return <WiDaySunnyOvercast />;
+    } else if (type.includes("snow")) {
+      return <WiSnowflakeCold />;
+    } else if (type.includes("sunny")) {
+      return <WiDaySunny />;
+    } else if (type.includes("showers") || type.includes("rain")) {
+      return <WiDayShowers />;
+    }
+  };
   render() {
     console.log(this.props.weather);
     return (
       <React.Fragment>
         {this.props.weather ? (
-          <h1> {this.props.weather.currently.apparentTemperature}°</h1>
+          <h1>
+            {" "}
+            {this.props.weather.currently.temperature}° {this.chooseIcon()}
+          </h1>
         ) : (
           "Loading Weather..."
         )}
